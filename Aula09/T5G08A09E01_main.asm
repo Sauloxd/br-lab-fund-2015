f45d4c3a380d07835e7d703aac6e44c691c04425
sauloxd <saulotoshi@gmail.com>
2015-11-12 16:26:14 -0200

    updated 09

--- Aula09/T5G08A09E01_main.asm
+++ Aula09/T5G08A09E01_main.asm
@@ -2,8 +2,25 @@
 LOADER          <
 LOADER_UL       <
 DUMPER			<
-DUMPER_UL   	<
-
+DUMP_UL   	<
+DUMP_BL <
+DUMP_INI <
+DUMP_TAM <
+DUMP_EXE <
+dadoLeituraTemp <
+argVrfTemp <
+hDASH <
+hCJB <
+hCDU <
+hCLO <
+hEND <
+hBRAN <
+h0001 <
+h0002 <
+h0003 <
+h0004 <
+h0000 <
+hD300 <
 
  
 		& 	/0000
@@ -12,34 +29,36 @@ UL		K 	/0000 	; parâmetro: UL onde está o arquivo de batch
 
 
 INI		LD UL 		; coloque aqui o código do seu monitor batch
-		SC READ
+		SC READ1
 		MM dadoLeituraTemp
 		- hDASH
 		JZ INIOK1 ;redireciona para continuar se for um //.
 		JP ERR01  ;processa erro de falta de início do JOB. Numerado de acordo com o enunciado.
 				
-INIOK1	SC READ
+INIOK1	SC READ1
 		MM dadoLeituraTemp
 		- hCJB
 		JZ LOOPRD
 		JP ERR01  ;processa erro de falta de início do JOB. Numerado de acordo com o enunciado.
 		
-LOOPRD	SC READ		;PULAR LINHA
-		SC READ		;ler próxima instrução
-		MM dadoLeituraTemp
+LOOPRD	SC READ1		;PULAR LINHA
+		SC READ1		;ler próxima instrução
+		MM dadoLeituraTemp ;temp
 		- hEND	
 		JZ FIMOK ; Se tiver o /*, sai com sucesso.
 		LD dadoLeituraTemp
-		-hDASH
+		- hDASH
 		JZ COMMOK ;Comando não reconhecido. Não tem o //.
 		JP ERR02
 		
-COMMOK	LD dadoLeituraTemp
-		- hCDU
-		JZ CALLDU ;Se for Dumper, chama o Dumper.
-		LD dadoLeituraTemp
-		- hCLO
-		JZ CALLLO ;Se for Loader, chama o Loader.
+COMMOK		SC READ1
+					MM dadoLeituraTemp
+					- hCDU
+					JZ CALLDU ;Se for Dumper, chama o Dumper.
+					LD dadoLeituraTemp
+					- hCLO
+					JZ CALLLO ;Se for Loader, chama o Loader.
+
 		
 		
 		;Saulo, como faremos aqui para ver o final do arquivo?
@@ -52,59 +71,59 @@ CALLLO	SC PROCL
 		
 		
 PROCD  	$ /0001
-		SC READ ;DESCARTA, é pular linha
-		SC READ	;lê argumento do Dumper 1-Tamanho bloco
-		SC VRFARG
+		SC READ1 ;DESCARTA, é pular linha (descarta eol)
+		SC READ1	;lê argumento do Dumper 1-Tamanho bloco
+		;SC VRFARG
 		MM DUMP_BL
-		SC READ ;RETIRA CARACTERES BRANCOS
-		SC READ ;lê argumento do Dumper 2-End. Inicial
-		SC VRFARG
+		SC READ1 ;RETIRA CARACTERES BRANCOS
+		SC READ1 ;lê argumento do Dumper 2-End. Inicial
+		;SC VRFARG
 		MM DUMP_INI
-		SC READ ;RETIRA CARACTERES BRANCOS
-		SC READ	;lê argumento do Dumper 3-Tamanho
-		SC VRFARG
+		SC READ1 ;RETIRA CARACTERES BRANCOS
+		SC READ1	;lê argumento do Dumper 3-Tamanho
+		;SC VRFARG
 		MM DUMP_TAM
-		SC READ ;RETIRA CARACTERES BRANCOS
-		SC READ ;lê argumento do Dumper 4-Endereço Início Execução
-		SC VRFARG
+		SC READ1 ;RETIRA CARACTERES BRANCOS
+		SC READ1 ;lê argumento do Dumper 4-Endereço Início Execução
+		;SC VRFARG
 		MM DUMP_EXE
-		SC READ ;RETIRA CARACTERES BRANCOS
-		SC READ ;PULA LINHA		
+		SC READ1 ;RETIRA CARACTERES BRANCOS
+		SC READ1 ;PULA LINHA		
 		SC DUMPER
 		RS PROCD
 		
 		
 		
 PROCL  	$ /0001
-		SC READ ;DESCARTA, é pular linha
-		SC READ	;lê argumento do Loader
-		SC VRFARG
+		SC READ1 ;DESCARTA, é pular linha
+		SC READ1	;lê argumento do Loader
+		;SC VRFARG
 		MM LOADER_UL
 		SC LOADER
 		RS PROCL
 
 ;VERIFICA SE O QUE ESTÁ NO ARGUMENTO É UM ARGUMENTO
 VRFARG	$ /0001	
-		MM argVrfTemp
-		- hBRAN
-		JZ ERR03
-		LD argVrfTemp
-		- hDASH
-		JZ ERR03
-		LD argVrfTemp
-		- hCJB
-		JZ ERR03
-		LD argVrfTemp
-		- hEND
-		JZ ERR03
-		LD argVrfTemp
-		- hCDU
-		JZ ERR03
-		LD argVrfTemp
-		- hCLO
-		JZ ERR03
-		LD argVrfTemp
-		RS VRFARG
+				MM argVrfTemp
+				- hBRAN
+				JZ ERR03
+				LD argVrfTemp
+				- hDASH
+				JZ ERR03
+				LD argVrfTemp
+				- hCJB
+				JZ ERR03
+				LD argVrfTemp
+				- hEND
+				JZ ERR03
+				LD argVrfTemp
+				- hCDU
+				JZ ERR03
+				LD argVrfTemp
+				- hCLO
+				JZ ERR03
+				LD argVrfTemp
+				RS VRFARG
 		
 		
 ERR01	LD h0001
@@ -136,11 +155,11 @@ FIMOK	LD h0000
 FIM 	HM	FIM		; fim do programa
 
 
-READ        $ /0001
+READ1        $ /0001
             LD hD300
-            +  LOADER_UL
-             MM ld_ins0
+            +  UL
+            MM ld_ins0
 ld_ins0     $ /0001 ;LEU primeira palavra : end inicial
-            RS READ
+            RS READ1
 
 # MAIN
